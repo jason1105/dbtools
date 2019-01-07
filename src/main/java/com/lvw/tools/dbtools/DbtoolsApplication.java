952336a7b0f1e4b01a6f5b06128be5fd793976c8
lv-wei <lvwei5@lenovo.com>
2019-01-07 14:15:54 +0800

    Initial commit

---
+++ src/main/java/com/lvw/tools/dbtools/DbtoolsApplication.java
@@ -0,0 +1,14 @@
+package com.lvw.tools.dbtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbtoolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbtoolsApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/lvw/tools/dbtools/DbtoolsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.lvw.tools.dbtools;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DbtoolsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
